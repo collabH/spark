ac151ffa556fdd711d6eba148bcae9869f185874
Shiming Huang <h1261109615@qq.com>
2020-09-20 21:36:16 +0800

    feat: read PartitionedPairBuffer

--- core/src/main/scala/org/apache/spark/util/collection/PartitionedPairBuffer.scala
+++ core/src/main/scala/org/apache/spark/util/collection/PartitionedPairBuffer.scala
@@ -19,7 +19,6 @@ package org.apache.spark.util.collection
 
 import java.util.Comparator
 
-import org.apache.spark.unsafe.Platform
 import org.apache.spark.unsafe.array.ByteArrayMethods
 import org.apache.spark.util.collection.WritablePartitionedPairCollection._
 
@@ -30,8 +29,8 @@ import org.apache.spark.util.collection.WritablePartitionedPairCollection._
  * The buffer can support up to 1073741819 elements.
  */
 private[spark] class PartitionedPairBuffer[K, V](initialCapacity: Int = 64)
-  extends WritablePartitionedPairCollection[K, V] with SizeTracker
-{
+  extends WritablePartitionedPairCollection[K, V] with SizeTracker {
+
   import PartitionedPairBuffer._
 
   require(initialCapacity <= MAXIMUM_CAPACITY,
@@ -40,44 +39,59 @@ private[spark] class PartitionedPairBuffer[K, V](initialCapacity: Int = 64)
 
   // Basic growable array data structure. We use a single array of AnyRef to hold both the keys
   // and the values, so that we can sort them efficiently with KVArraySortDataFormat.
+  // cap容量默认为64
   private var capacity = initialCapacity
+  // 当前集合大小
   private var curSize = 0
+  // 数组，2被的cap
   private var data = new Array[AnyRef](2 * initialCapacity)
 
   /** Add an element into the buffer */
   def insert(partition: Int, key: K, value: V): Unit = {
+    // 如果超过容量
     if (curSize == capacity) {
       growArray()
     }
+    // key存储一个tuple2为 (partition,key)
     data(2 * curSize) = (partition, key.asInstanceOf[AnyRef])
+    // 设置value
     data(2 * curSize + 1) = value.asInstanceOf[AnyRef]
     curSize += 1
+    // 记录采样
     afterUpdate()
   }
 
   /** Double the size of the array because we've reached capacity */
   private def growArray(): Unit = {
+    // check cap
     if (capacity >= MAXIMUM_CAPACITY) {
       throw new IllegalStateException(s"Can't insert more than ${MAXIMUM_CAPACITY} elements")
     }
+    // 计算新的cap，2*oldCap
     val newCapacity =
       if (capacity * 2 > MAXIMUM_CAPACITY) { // Overflow
         MAXIMUM_CAPACITY
       } else {
         capacity * 2
       }
+    // 计算新的数组
     val newArray = new Array[AnyRef](2 * newCapacity)
+    // 数组拷贝
     System.arraycopy(data, 0, newArray, 0, 2 * capacity)
     data = newArray
     capacity = newCapacity
+    // 重新采样
     resetSamples()
   }
 
+  // 分区排序
   /** Iterate through the data in a given order. For this class this is not really destructive. */
   override def partitionedDestructiveSortedIterator(keyComparator: Option[Comparator[K]])
-    : Iterator[((Int, K), V)] = {
+  : Iterator[((Int, K), V)] = {
     val comparator = keyComparator.map(partitionKeyComparator).getOrElse(partitionComparator)
+    // 排序
     new Sorter(new KVArraySortDataFormat[(Int, K), AnyRef]).sort(data, 0, curSize, comparator)
+    // 返回迭代器
     iterator
   }
 
